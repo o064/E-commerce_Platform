9eaa135e83efa25f267a5889badcd3ff6cd7b53e
omar <om039919@gmail.com>
2025-09-27 16:57:48 +0300

    setup frontEnd

---
+++ SwiftBuy/src/App.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+
+
+const router = createBrowserRouter([
+  {
+
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
